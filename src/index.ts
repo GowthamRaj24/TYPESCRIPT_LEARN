418335ce9786683bd2422f104bd23fab230fdbac
Gowtham Raj <gmanda@gitam.in>
2024-12-07 22:03:30 +0530

    learnt connection pooling

--- src/index.ts
+++ src/index.ts
@@ -11,8 +11,34 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
+
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
+		const prisma = new PrismaClient().$extends(withAccelerate())
+
+		const response = await prisma.log.create({
+			data : {
+			level : "10",
+			message : "Hello World",
+			meta : {
+				headers : JSON.stringify(request.headers),
+			}
+		}
+		})
+
+		const logs = await prisma.log.findMany({
+			take : 10,
+			orderBy : {
+				id : 'desc',
+			},
+		})
+
+		console.log(JSON.stringify(logs));
+
+
+		return  Response.json(response);
 	},
 } satisfies ExportedHandler<Env>;
+
